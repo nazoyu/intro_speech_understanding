54a026a2c7ed135f7a8465c55821a667803708b5
nazoyu <74831633+nazoyu@users.noreply.github.com>
2025-02-01 07:19:49 +0900

    Update homework14.py

--- 2024_fall/lec14/homework14.py
+++ 2024_fall/lec14/homework14.py
@@ -8,7 +8,18 @@ def what_time_is_it(lang, filename):
     lang (str) - language in which to speak
     filename (str) - the filename into which the audio should be recorded
     '''
-    raise RuntimeError("You need to write this part!")
+    #raise RuntimeError("You need to write this part!")
+    (date,time) = datetime.datetime.now().isoformat().split("T")
+    (hour, minutes, seconds) = time.split(":")
+    if lang=="en":
+        text = hour+" hours and "+minutes+" minutes"
+    elif lang=="ja" :
+        text = hour+"時"+minutes+"分です"
+    elif lang=="zh" :
+        text = "现在是"+hour+"点"+"分"
+    else:
+        text="I"m sorry, I don't know that language"
+    gtts.gTTS(text,lang=lang).save(filename)
     
 def tell_me_a_joke(lang, audiofile):
     '''
@@ -19,7 +30,13 @@ def tell_me_a_joke(lang, audiofile):
     lang (str) - language
     audiofile (str) - audiofile in which to record the joke
     '''
-    raise RuntimeError("You need to write this part!")
+    #raise RuntimeError("You need to write this part!")
+    filename = 'jokes_%s.txt'%(lang)
+    with open(filename) as f:
+        jokes = f.readlines()
+    joke = random.choice(jokes)
+    print(joke.strip())
+    gtts.gTTS(joke.strip(), lang=lang).save(audiofile)
 
 def what_day_is_it(lang, audiofile):
     '''
@@ -32,7 +49,25 @@ def what_day_is_it(lang, audiofile):
     @returns:
     url (str) - URL that you can look up in order to see the calendar for this month and year
     '''
-    raise RuntimeError("You need to write this part!")
+    #raise RuntimeError("You need to write this part!")
+    today = datetime.date.today()
+    year = today.year
+    month = today.month
+    day = today.day
+    weekday = today.isoweekday()
+    if lang=="en" :
+        weekdays=['','Monday','Tuesday','wednesday','Thursday','Friday','Saturday','Sunday']
+        months=['','January','February','March','April','May','June','July','August','September','October','November','December']
+        text = "%s,%s %d,%d"%(weekdays [weekday], months[month],day,year)
+        gtts.gTTS("Today is"+text,lang="en").save(audiofile)
+    elif lang=="ja":
+        weekdays='月火水木金土日'
+        text="%s曜日,%d月%d日,%d年"%(weekdays [weekday],month, day, year)
+        gtts.gTTS("今日は"+text,lang="ja").save(audiofile)
+    elif lang=="zh":
+        weekdays=['','周一','周二','周三','周四','周五','周六','星期日']
+        text='%s,%d月d日,%d年'%(weekdays [weekday],month,day,year)
+        gtts.gTTS("今天是"+text,lang="zh").save(audiofile)
 
 def personal_assistant(lang, filename):
     '''
